896ee1709ed42f5fa5a133e927243bf23d791bb1
Coder_rachit81 <95204792+Coder519@users.noreply.github.com>
2021-12-06 20:20:56 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function getData() {
+    firebase.database().ref("/").on('value', function (snapshot) {
+        document.getElementById("output").innerHTML = ""; snapshot.forEach(function (childSnapshot) {
+            childKey = childSnapshot.key;
+            Room_names = childKey;
+            //Start code
+
+            //End code
+        });
+    });
+}
+getData();
